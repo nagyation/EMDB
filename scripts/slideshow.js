720370e68475f44a6db2f097799d7fdeed850b7e
Mahmoud Nagy <mnagy1312@gmail.com>
2018-01-25 15:06:55 +0200

    sperated js from html

---
+++ scripts/slideshow.js
@@ -0,0 +1,41 @@
+var slideIndexRear = 0;
+var slideIndexFront = 2;
+var SLIDES_COUNT = 3;
+var currentSlides= ["","",""];
+var slidesSrc = ["img/s1.jpg","img/s2.jpg","img/s3.jpg","img/s4.jpg","img/s5.jpg"];
+var i;
+for(i=0; i < SLIDES_COUNT ; i++)
+		currentSlides[i] = slidesSrc[(slideIndexRear+i) % slidesSrc.length];
+showSlides();
+
+function plusSlides(n)
+{
+	 	var i;
+
+		if(n > 0)
+		{
+			  for(i = SLIDES_COUNT-1; i >0;i--)
+				    currentSlides[i] = currentSlides[i-1];
+			  slideIndexRear= (slideIndexRear - 1 + slidesSrc.length) %slidesSrc.length;
+			  slideIndexFront= (slideIndexFront - 1 + slidesSrc.length) %slidesSrc.length;
+			  currentSlides[i] = slidesSrc[slideIndexRear];
+			  
+		}
+		else{
+			  for(i = 0; i < SLIDES_COUNT-1;i++)
+				    currentSlides[i] = currentSlides[i+1];
+			  slideIndexRear = (slideIndexRear + 1) %slidesSrc.length;
+			  slideIndexFront = (slideIndexFront + 1) %slidesSrc.length;
+			  currentSlides[i] = slidesSrc[slideIndexFront];
+		}
+		
+	  showSlides();
+}
+
+function showSlides() {
+	  var i;
+	  var slides = document.getElementsByClassName("cover-photo");
+
+	  for(i = 0; i < SLIDES_COUNT ; i ++)
+	  	  slides[i].src = currentSlides[i] ;  
+}
