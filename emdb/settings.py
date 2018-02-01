857b7f77f2eaaf06b548d17c737cae82a4daa69d
Mahmoud Nagy <mnagy1312@gmail.com>
2018-02-01 13:21:47 +0200

    created model review

--- emdb/settings.py
+++ emdb/settings.py
@@ -33,6 +33,7 @@ ALLOWED_HOSTS = []
 INSTALLED_APPS = [
     'movies.apps.MoviesConfig',
     'registeration.apps.RegisterationConfig',
+    'review',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
