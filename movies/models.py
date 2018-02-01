09399a1d3b911889943e6cc668eef83215ab5673
Unknown <mohamed_medhat1996@yahoo.com>
2018-02-01 02:11:36 +0200

    movie model

--- movies/models.py
+++ movies/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+class Movie (models.Model):
+    name = models.CharField(max_length=250)
+    production_date = models.CharField(max_length=100)
+    writer = models.CharField(max_length=250)
+    director = models.CharField(max_length=250)
+    description = models.CharField(max_length=10000)
+    rate = models.CharField(max_length=100)
+    trailer_url = models.CharField(max_length=1000)
+    movie_logo = models.CharField(max_length=1000)
+
+    def __str__(self):
+        return self.name+' - '+ self.production_date
+
