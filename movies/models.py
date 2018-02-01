0f5379b4199e74d735b3eb7da44d7017db5bc3df
Mahmoud Nagy <mnagy1312@gmail.com>
2018-02-01 17:10:44 +0200

    home is done

--- movies/models.py
+++ movies/models.py
@@ -1,15 +1,15 @@
 from django.db import models
 
 class Movie (models.Model):
-    name = models.CharField(max_length=250)
-    production_date = models.CharField(max_length=100)
-    writer = models.CharField(max_length=250)
-    director = models.CharField(max_length=250)
-    description = models.CharField(max_length=10000)
-    rate = models.CharField(max_length=100)
-    trailer_url = models.CharField(max_length=1000)
-    movie_logo = models.CharField(max_length=1000)
+	name = models.CharField(max_length=250)
+	production_date = models.DateField()
+	writer = models.CharField(max_length=250)
+	director = models.CharField(max_length=250)
+	description = models.CharField(max_length=10000)
+	rate = models.IntegerField()
+	trailer_url = models.CharField(max_length=1000)
+	movie_logo = models.CharField(max_length=1000)
+	# genre =
 
-    def __str__(self):
-        return self.name+' - '+ self.production_date
-
+	def __str__(self):
+		return self.name+' - '+ str(self.production_date)
--- movies/views.py
+++ movies/views.py
@@ -1,13 +1,19 @@
 from django.shortcuts import render
 from django.views.generic import TemplateView
-# Create your views here.
+from movies.models import Movie
+from django.shortcuts import render
 
+# Create your views here.
 class HomeView(TemplateView):
 	template_name = "movies/home.html"
+	def get(self,request):
+		latest_movies =  list(Movie.objects.order_by("production_date").values_list('movie_logo',flat=True)[:5])
+		print(latest_movies)
+		return render(request,self.template_name, {'latest_movies': latest_movies})
 
 class MovieView(TemplateView):
 	template_name = "movies/moviepage.html"
-	
+
 class BrowseView(TemplateView):
 	template_name = "movies/browse.html"
-	
+
