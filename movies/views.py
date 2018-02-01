498f32627b6f1c2e5975c4afbabf8bcc542bf600
Unknown <mohamed_medhat1996@yahoo.com>
2018-02-01 18:47:21 +0200

    movies logo in browse page

--- movies/views.py
+++ movies/views.py
@@ -1,13 +1,22 @@
 from django.shortcuts import render
-from django.views.generic import TemplateView
-# Create your views here.
+from django.views.generic import TemplateView , ListView
+from movies.models import Movie
+from django.shortcuts import render
 
+# Create your views here.
 class HomeView(TemplateView):
 	template_name = "movies/home.html"
+	def get(self,request):
+		latest_movies =  list(Movie.objects.order_by("production_date").values_list('movie_logo',flat=True)[:5])
+		print(latest_movies)
+		return render(request,self.template_name, {'latest_movies': latest_movies})
 
 class MovieView(TemplateView):
 	template_name = "movies/moviepage.html"
-	
-class BrowseView(TemplateView):
+
+class BrowseView(ListView):
 	template_name = "movies/browse.html"
-	
+	def get(self,request):
+		all_movies =  Movie.objects.all()
+		return render(request,self.template_name, {'all_movies': all_movies})
+
