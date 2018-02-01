651ea272e5bb1208ade3962f5bc7a2358595b006
Mahmoud Nagy <mnagy1312@gmail.com>
2018-02-01 13:28:29 +0200

    added review model

--- review/models.py
+++ review/models.py
@@ -1,11 +1,11 @@
 from django.db import models
-#from movies.models import Movie
-#from registeration.models import User
+from movies.models import Movie
+from registeration.models import User
 
 # Create your models here.
 
 class Review(models.Model):
     rate = models.IntegerField()
     comment = models.CharField(max_length=10000)
-    #user = models.ForeignKey(User,on_delete=models.CASCADE)
-    #movie = models.ForeignKey(Movie,on_delete=models.CASCADE)
+    user = models.ForeignKey(User,on_delete=models.CASCADE)
+    movie = models.ForeignKey(Movie,on_delete=models.CASCADE)
