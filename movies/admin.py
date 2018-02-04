e30d7cdcb472ca01fc47460c02053127ca120321
Unknown <mohamed_medhat1996@yahoo.com>
2018-02-04 17:28:53 +0200

    some handle in moviepage

--- movies/admin.py
+++ movies/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
-from .models import Movie , Actor
+from .models import Movie , Actor , Director ,Writer
 
 admin.site.register(Movie)
 admin.site.register(Actor)
+admin.site.register(Director)
+admin.site.register(Writer)
--- movies/urls.py
+++ movies/urls.py
@@ -4,10 +4,8 @@ from . import views
 
 app_name = 'movies'
 urlpatterns = [
-
 	path('', HomeView.as_view(), name='home'),
 	path('home', HomeView.as_view()),
 	path('browse', BrowseView.as_view(), name='browse'),
 	path('movie', MovieView.as_view(), name='movie'),
-
 ]
