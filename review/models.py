cde2b1dca53055a98e54a9973df986df079b92e1
Mahmoud Nagy <mnagy1312@gmail.com>
2018-02-05 22:58:12 +0200

    removed wrong user model

--- review/models.py
+++ review/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 from movies.models import Movie
-from django.contrib.auth.models import User
+from registeration.models import User
 from django.core.validators import MinValueValidator, MaxValueValidator
 
 # Create your models here.
