e9a80a06dec5c59694b70a874ad5021bc85ad076
Mahmoud Nagy <mnagy1312@gmail.com>
2018-04-28 20:22:22 +0200

    finished review

--- review/views.py
+++ review/views.py
@@ -6,6 +6,7 @@ from movies.models import Movie
 from review.forms import ReviewForm
 from registeration.models import User
 from django.db import IntegrityError
+from django.db import connection
 
 class ReviewCreate(View):
 
@@ -13,15 +14,22 @@ class ReviewCreate(View):
         form = ReviewForm(request.POST)
         if(form.is_valid()):
             review = form.save(commit=False)
-            review.movie = Movie.objects.get(pk=kargs['movie_pk'])
-            review.created_by = User.objects.get(id=request.user.id)
+            #review.movie = Movie.objects.get(pk=kargs['movie_pk'])
+            review.movie = Movie.objects.raw("SELECT * FROM movies_movie where id = %s", [kargs['movie_pk']])[0]
+            #review.created_by = User.objects.get(id=request.user.id)
+            review.created_by = User.objects.raw("SELECT * FROM registeration_user where id =%s", [request.user.id])[0]
             try:
                 review.save()
             except IntegrityError as e:
-                old_review = Review.objects.filter(movie=review.movie,created_by=review.created_by)[0]
-                old_review.comment = review.comment
-                old_review.rate = review.rate
-                old_review.save()
+                #old_review = Review.objects.filter(movie=review.movie,created_by=review.created_by)[0]
+                #old_review.comment = review.comment
+                #old_review.rate = review.rate
+                #old_review.save()
+                print("Allaahu akbaar")
+                with connection.cursor() as cursor:
+                    cursor.execute("UPDATE review_review SET comment = %s, rate = %s\
+                                   WHERE movie_id = %s AND created_by_id = %s",
+                                   [review.comment,review.rate,review.movie.id,review.created_by.id])
             return redirect("movies:movie", kargs['movie_pk'], )
 
 
